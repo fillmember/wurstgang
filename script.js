ddf9c3a0905f333ba49e7ec73cc236e90d93e266
Glitch (lively-glitter) <none>
2018-02-23 17:42:32 +0000

    👟🍜 Checkpoint script.js:700520/654

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var THREE = window.THREE;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+var dog = null;
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -14,13 +16,12 @@ loader.load(
   'https://cdn.glitch.com/c03493ab-dd08-4537-a1ff-7a40876e881f%2Fq1.glb?1519406826712',
   // called when the resource is loaded
   function ( gltf ) {
-    
-    console.log(gltf)
-
-    scene.add( gltf.scene );
-    
-    renderer.render(scene,camera)
-
+    scene.add( gltf.scene.children[0] );
+    dog = scene.children[0];
+    dog.position.z = -10
+    // set dog material
+    var unlit = new THREE.MeshBasicMaterial();
+    console.log(dog.children[0].material)
   },
   // called when loading is in progresses
   function ( xhr ) {
@@ -35,3 +36,12 @@ loader.load(
 
   }
 );
+
+function animate() {
+  if (dog) {
+    dog.rotateY(-0.1);
+  }
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
