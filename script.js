093c34ebd0c27d4b1d780cbe950fdd182ab05e57
Glitch (lively-glitter) <none>
2018-02-23 17:32:09 +0000

    🏀🐪 Checkpoint script.js:700520/392 index.html:700520/128 GLTFLoader.js:700520/3

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('hi');
+var THREE = window.THREE;
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var loader = new THREE.GLTFLoader();
+
+loader.load(
+  // resource URL
+  'https://cdn.glitch.com/c03493ab-dd08-4537-a1ff-7a40876e881f%2Fq1.glb?1519406826712',
+  // called when the resource is loaded
+  function ( gltf ) {
+    
+    console.log(gltf)
+
+    scene.add( gltf.scene );
+    
+    renderer.render(scene,camera)
+
+  },
+  // called when loading is in progresses
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+  },
+  // called when loading has errors
+  function ( error ) {
+
+    console.log( 'An error happened' , error );
+
+  }
+);
