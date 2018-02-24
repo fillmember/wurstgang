365dd44e90f16f322352198caac0192149ff945f
Glitch (lively-glitter) <none>
2018-02-24 15:38:01 +0000

    🚙🎣 Checkpoint script.js:700520/262

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var THREE = window.THREE;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var dog,mesh,skele;
+var dog,skele;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0xFFFFFF);
@@ -21,19 +21,20 @@ loader.load(
     dog = scene.getObjectByName("Mesh");
     dog.position.z = -7
     dog.position.y = 0
-    mesh = dog.getObjectByName("Mesh");
-    skele = mesh.skeleton
+    skele = dog.skeleton
     // set dog material
     var unlit = new THREE.MeshBasicMaterial({
       color: 0xFFFFFF,
-      map: mesh.material.map
+      map: dog.material.map,
+      skinning: true
     });
     unlit.map.encoding = THREE.LinearEncoding
     unlit.needsUpdate = true;
-    mesh.material = unlit;
+    dog.material = unlit;
+    //
     dog.rotateY(3.14159);
     // Skele Helper
-    var helper = new THREE.SkeletonHelper( mesh );
+    var helper = new THREE.SkeletonHelper( dog );
     helper.material.linewidth = 3;
     scene.add( helper );
   },
@@ -52,9 +53,10 @@ loader.load(
 );
 
 function animate() {
+  var time = Date.now() * 0.01;
   if (dog) {
-
-    dog.rotateY(-0.003);
+    
+    THREE.Math.degToRad( 39.231 + Math.sin(time) * 10 )
 
   }
 	requestAnimationFrame( animate );
