f5227539ec6c09c078d4362a1553daadf4718cd0
Glitch (wurstgang) <none>
2018-02-24 16:29:21 +0000

    👨🎗 Checkpoint script.js:700520/763 styles.css:700520/22

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var THREE = window.THREE;
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var dog,skele;
-
+camera.position.set( 0 , 0 , 10 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setClearColor( 0x248B4D );
@@ -12,15 +9,26 @@ renderer.setSize( window.innerWidth , window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var controls = new THREE.OrbitControls( camera , renderer.domElement );
+controls.autoRotate = true
+controls.autoRotateSpeed = 0.1
+controls.enableDamping = true
+controls.dampingFactor = 0.5
+controls.enablePan = false
+controls.enableZoom = false
+controls.minPolarAngle = 0.15;
+controls.maxPolarAngle = 3.0;
+
+var dog,skele,bones;
 
 var loader = new THREE.GLTFLoader();
-
 loader.load(
   'https://cdn.glitch.com/c03493ab-dd08-4537-a1ff-7a40876e881f%2Fwt.glb?1519485752419',
   function ( gltf ) {
     scene = gltf.scene;
     dog = scene.getObjectByName("Mesh");
+    dog.position.y = -0.5;
     skele = dog.skeleton
+    bones = skele.bones
     // set an unlit material
     var unlit = new THREE.MeshBasicMaterial({
       color: 0xFFFFFF,
@@ -33,9 +41,9 @@ loader.load(
     // Pose Init
     // init a very cute pose
     dog.rotateY(3*0.75)
-    skele.bones[5].rotateY(3/8)
-    skele.bones[6].rotateY(1/8)
-    skele.bones[8].rotateY(3/4)
+    bones[5].rotateY(3/8)
+    bones[6].rotateY(1/8)
+    bones[8].rotateY(3/4)
   },
   function ( xhr ) {
     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -45,19 +53,13 @@ loader.load(
   }
 );
 
-camera.position.set( 0 , 4 , 15 );
-
-var wagging = new TimelineMax({repeat:-1});
-
-
 function animate() {
   var time = Date.now() * 0.01;
   if (dog) {
-
-    skele.bones[1].rotation.y = THREE.Math.degToRad( Math.sin(time) * 5 )
-    skele.bones[2].rotation.y = THREE.Math.degToRad( Math.sin(time) * -20 )
-    skele.bones[3].rotation.y = THREE.Math.degToRad( Math.sin(time) * -30 )
-
+    // Wagging
+    bones[1].rotation.y = THREE.Math.degToRad( Math.sin(time) * 5 )
+    bones[2].rotation.y = THREE.Math.degToRad( Math.sin(time) * -20 )
+    bones[3].rotation.y = THREE.Math.degToRad( Math.sin(time) * -30 )
   }
   controls.update();
 	requestAnimationFrame( animate );
