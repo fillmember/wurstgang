eeeadf9283a6c3d433e90ac2787e98a93d640363
ten <ten@mutate.space>
2018-07-08 20:52:53 +0200

    use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,8 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"]
 };
